269172bb81ccdb6df9d471aa0fc47bba74e77ec8
Konstantin Chaika <pro100kot14@gmail.com>
2022-04-20 12:07:53 -0500

    test for cnn

--- BlinkDetection.py
+++ BlinkDetection.py
@@ -3,6 +3,7 @@ import cv2
 import dlib
 import math
 import nanocamera as nano
+import time
 
 BLINK_RATIO_THRESHOLD = 5.0 #5.7
 
@@ -52,9 +53,10 @@ def rect_to_bb(rect):
 #cap = cv2.VideoCapture(1)
 #pipeline = 'nvarguscamerasrc !  video/x-raw(memory:NVMM), width=1920, height=1080, format=NV12, framerate=30/1 ! nvvidconv flip-method=0 ! video/x-raw, width=1920, height=1080, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
 #cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
-#cap = cv2.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink' , cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=(fraction)120/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink' , cv2.CAP_GSTREAMER)
+#gst-launch-1.0 nvarguscamerasrc ! 'video/x-raw(memory:NVMM), width=1280, height=720, forat=NV12, framerate=120/1' ! fakesink
 #print(cap)
-camera = nano.Camera(camera_type=0, device_id=0, width=1640, height=1232, fps=20, debug=False, flip=2)
+#camera = nano.Camera(camera_type=0, device_id=0, width=1640, height=1232, fps=20, debug=False, flip=2)
 #3264 x 2464 
 #1640 x 1232
 
@@ -67,28 +69,29 @@ cv2.namedWindow('BlinkDetector')
 
 #-----Step 3: Face detection with dlib-----
 detector = dlib.get_frontal_face_detector()
-#cnn_face_detector = dlib.cnn_face_detection_model_v1("shape_predictor_68_face_landmarks.dat")
+cnn_face_detector = dlib.cnn_face_detection_model_v1("mmod_human_face_detector.dat")
 
 
 
 #-----Step 4: Detecting Eyes using landmarks in dlib-----
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+#predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 #these landmarks are based on the image above 
 left_eye_landmarks  = [36, 37, 38, 39, 40, 41]
 right_eye_landmarks = [42, 43, 44, 45, 46, 47]
-
+t = time.time()
 while True:
     #capturing frame
 #    print('cap.read():')
-#    retval, frame = cap.read()
-    frame = camera.read()
+    
+    retval, frame = cap.read()
+#    frame = camera.read()
 
     #exit the application if frame not found
 #    if not retval:
 #        print("Can't receive frame (stream end?). Exiting ...")
 #        break 
 
-    frame = cv2.resize(frame, (410,308), interpolation = cv2.INTER_AREA)
+    frame = cv2.resize(frame, (640,360), interpolation = cv2.INTER_AREA)
 
     #-----Step 2: converting image to grayscale-----
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -96,11 +99,14 @@ while True:
 
     #-----Step 3: Face detection with dlib-----
     #detecting faces in the frame 
-    faces,_,_ = detector.run(image = frame, upsample_num_times = 0, adjust_threshold = 0.0)
- #   dets = cnn_face_detector(image, 1)
-#    print(faces)
-
+    t = time.time()
+#    faces,_,_ = detector.run(image = frame, upsample_num_times = 0, adjust_threshold = 0.0)
+    dets = cnn_face_detector(frame, 1)
+    print(dets)
+    cur = time.time()
+    print(cur-t)
     #-----Step 4: Detecting Eyes using landmarks in dlib-----
+    """
     for face in faces:
         
         landmarks = predictor(frame, face)
@@ -118,11 +124,11 @@ while True:
             cv2.putText(frame,"BLINKING",(10,50), cv2.FONT_HERSHEY_SIMPLEX,
             2,(255,255,255),2,cv2.LINE_AA)
 
-
-    cv2.imshow('BlinkDetector', frame)
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+"""
+#    cv2.imshow('BlinkDetector', frame)
+#    key = cv2.waitKey(1)
+#    if key == 27:
+#        break
 
 #releasing the VideoCapture object
 cap.release()
